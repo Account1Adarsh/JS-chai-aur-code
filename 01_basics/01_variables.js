849b8208ef59efd3f7b43d0cba9b8344e2765ea8
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-14 09:26:14 +0000

    01_basics_addition

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const account_Id="12345678"
+let account_mail="aka@gmail.com"
+var account_password="pass123"
+account_City="Jaipur"
+
+console.log(account_Id);
+
+account_mail="bkb@gmail.com";
+account_password="newpass123";
+account_City="Banglore";
+
+// Use let for variable declaration 
+
+// we can declare and not itialize as well a varialbe in js just like c++;
+
+
+console.table([account_Id,account_City,account_mail,account_password]);
+
+
