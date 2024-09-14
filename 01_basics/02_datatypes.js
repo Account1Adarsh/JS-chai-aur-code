6f768a9c457d4369da65109abfc58dcc80282729
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-14 09:40:31 +0000

    data types

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict";
+
+// alert(3+3) we are on nodejs not on browser
+
+console.log(3+3);
+console.log("yo hello");
+
+
+let name="Adarsh";
+let age="12";
+let isadult=true;
+
+//string => ""
+// int => 2 power 53
+// boolean=> true/false
+//null -> stand alone value
+//undifined =>
+//symbol => to find uniquiness
+
+
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
