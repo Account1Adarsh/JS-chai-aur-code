886298615fce8bacc587dd63a1d817983e2bb44d
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-14 11:26:09 +0000

    new lecture

---
+++ 01_basics/04_compare.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2==1);
+// console.log(2!=1);
+// console.log(2<=1);
+// console.log(2 < 1);
+
+// ====  is strict check means it also check its data type 
+
+console.log("2"==2);
+console.log("2"===2);
+
+
+
+
