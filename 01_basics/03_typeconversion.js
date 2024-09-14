838a2b62158b8e94e1a2bfb90c85c1f79fa86390
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-14 11:15:29 +0000

    operations in type conversion

--- 01_basics/03_typeconversion.js
+++ 01_basics/03_typeconversion.js
@@ -1,35 +1,77 @@
-let score="33"
+// let score="33"
 
-console.log(score);
-console.log(typeof score);
+// console.log(score);
+// console.log(typeof score);
 
-let value=Number(score)
+// let value=Number(score)
 
-console.log(typeof value)
+// console.log(typeof value)
 
-score="33abc"
-value=Number(score)
-console.log(value);
-console.log(typeof value)
+// score="33abc"
+// value=Number(score)
+// console.log(value);
+// console.log(typeof value)
 
-// "33" ->33
-//"33abs" ->NaN
-// null->
-//true->1
-//false->0
-//""->false;
-//10->true
-//"as"->true
-//0->false;
+// // "33" ->33
+// //"33abs" ->NaN
+// // null->
+// //true->1
+// //false->0
+// //""->false;
+// //10->true
+// //"as"->true
+// //0->false;
 
 
-score="no"
-value=Boolean(score)
-console.log(value);
+// score="no"
+// value=Boolean(score)
+// console.log(value);
 
-let s="20"
-let num=Number(s);
+// let s="20"
+// let num=Number(s);
 
-console.log(typeof num);
-console.log(num);
+// console.log(typeof num);
+// console.log(num);
+
+/******************************Operations*******************************/
+
+
+let value=3
+let negvalue=-value
+
+// console.log(negvalue);
+
+// 2**6 is 2 power 6 
+// % is for reminder
+// / is for division
+
+// let str1="Hello "
+// let str2="World"
+// let str3= str1+str2//concation is also present here
+// console.log(str3);
+
+// console.log("1"+2);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(1+"2");
+
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1=num2=num3=4
+// console.table([num1,num2,num3]);
+
+
+let gamecounter=100;
+gamecounter++
+--gamecounter
+console.log(gamecounter);
+
+
+
+
+
+
 
