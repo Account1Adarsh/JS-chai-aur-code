579662ae2e4275205841bbf6bb4ac2e72882d3d5
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-15 10:48:04 +0000

    array

--- 02_basics/01_array.js
+++ 02_basics/01_array.js
@@ -5,3 +5,37 @@ console.log(myarr[0]);
 const myarr2=new Array(1,2,3,4);
 console.log(myarr2[3]);
 
+
+// Array methods
+
+// myarr.push(6);
+// myarr.push(7);
+// console.log(myarr);
+// myarr.pop();
+// console.log(myarr);
+
+
+// // array unshift timecomplexity O(n); To add element in front
+
+// myarr.unshift(12);//push front
+// console.log(myarr);
+
+// myarr.shift();//pop front
+// console.log(myarr);
+
+// console.log(myarr.includes(9));
+// console.log(myarr.indexOf(9));
+
+const newarr=myarr.join();
+console.log(myarr);
+console.log(newarr);
+
+
+
+//slice or splice
+
+
+
+
+
+
