5cb267da58c7ca333ce6dd475f69a6fadf223058
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-15 03:05:27 +0000

    Math and num

---
+++ 01_basics/06_number_math.js
@@ -0,0 +1,55 @@
+// const score=400
+
+// const temp= new Number(400);
+// console.log(score);
+// console.log(temp);
+
+// // .toString()
+// console.log(typeof temp.toString());
+
+// console.log(temp.toFixed(2));
+
+// const othernum= 23.8966
+
+// console.log(othernum.toPrecision(2));
+
+// const hundred=1000000;
+// console.log(hundred.toLocaleString());
+// console.log(hundred.toLocaleString('en-IN'));
+
+// console.log(Number.MAX_VALUE);
+
+
+// +++++++++++++++++++++Maths++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-23));
+// console.log(Math.round(4.499));
+// console.log(Math.ceil(4.01));
+// console.log(Math.floor(4.9));
+
+// console.log(Math.pow(2,8));
+// console.log(Math.min(1,4,6,90));
+// console.log(Math.max(2,8,12,23,45));
+
+console.log(Math.random()); //ranges from 0 to 1
+console.log(Math.random()*10);
+console.log(Math.random()*600);
+
+
+const min=10;
+const max=20;
+
+console.log(Math.floor(math.random()*(max-min+1))+min);
+
+
+
+
+
+
+
+
+
+
+
+
