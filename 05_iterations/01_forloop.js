5c9005eb5a349431cecbf5d334677a199e2d8c98
Adarsh Kumar Singh <142895773+Account1Adarsh@users.noreply.github.com>
2024-09-18 00:56:40 +0000

    loop

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,13 @@
+//for loop
+
+for(let k=1;k<10;k*=2){
+    console.log(k);
+}
+
+// for(let i=0;i<=10;i++){
+//     for(let j=0;j<=10;j++){
+//         console.log(i+" * " + j + " = "+i*j);
+//     }
+// }
+
+//break and continue
---
+++ 05_iterations/02_while.js
@@ -0,0 +1,17 @@
+// let index=0;
+
+// while(index<=10){
+//     console.log(`Value of index is ${index}`);
+//     index=index+2;
+// }
+
+// do{
+
+// }while(condition);
+
+let score=11;
+do{
+    console.log(`Value is ${score}`);
+    score++;
+    
+}while(score<=10);
